b6e61aa9719438749484e1a2ed8cf0abd664a173
Thomas M Thoppil <thomasthoppil98@gmail.com>
2021-10-19 03:08:44 +0530

    merge

--- app.py
+++ app.py
@@ -1,27 +1,47 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 13 12:38:13 2021
+
+@author: Thomas M Thoppil
+"""
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import pickle
-
+import lightgbm as lgb
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
-@app.route('/', methods = ['POST'])
-def home():
-    return render_template('index.html')
+model = lgb.Booster(model_file='mode.txt')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/')
+def welcome():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods = ['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    prediction = model.predict(final_features).round(0)
 
-    output = round(prediction[0], 2)
+    if int(prediction[0]) == 1:
+        output = 'legitimate'
+    else:
+        output = 'illegitmate'
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text=f'The predicted score is {prediction[0]} and hence the file is {output}')
 
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))]).round(0)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    output = prediction[0]
+    return jsonify(output)
+
+    
+    
+if __name__ == '__main__':
+    app.run()
