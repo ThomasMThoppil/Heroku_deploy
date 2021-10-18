189399c0564b16a4d8829ff8c8133620fb5673c6
Thomas M Thoppil <thomasthoppil98@gmail.com>
2021-10-19 04:13:49 +0530

    app_1 to app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app_1
+web: gunicorn app:app
--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 03:07:03 2021
+
+@author: Thomas M Thoppil
+"""
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -11,17 +17,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    return render_template('index.html', prediction_text = 'lol')
 
-    output = round(prediction[0], 2)
-
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
