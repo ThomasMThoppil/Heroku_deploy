736b3b6e9254027189548c071534512cb67bc17d
Thomas M Thoppil <thomasthoppil98@gmail.com>
2021-10-19 04:40:35 +0530

    model in

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ Created on Tue Oct 19 03:07:03 2021
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+import lightgbm as lgb
 
+model = lgb.Booster(model_file='lightGBMModel.txt')
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,7 +19,9 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    return render_template('index.html', prediction_text = 'lol')
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    return render_template('index.html', prediction_text = f'final__{final_features}')
 
 if __name__ == '__main__':
     app.run()
